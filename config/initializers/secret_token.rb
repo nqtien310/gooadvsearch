cff50563bcda70df3ffe50b76bf918e7e6a2a2f4
Tien Nguyen <nqtien310@localhost.localdomain>
2013-02-19 18:05:39 -0500

    App initialization with rspec included

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gooadvsearch::Application.config.secret_token = '9877f154adee60dd82ff8ae1d500779147d13beba78b2e4e86bb323285ce1950da3045a5c768cd70f970e66c57be197e5d5d9e5f305db4851698c0f659ddeb81'
